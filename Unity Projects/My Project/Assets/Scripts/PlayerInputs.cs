61b45f31f878a0af854b7a875f33a4bccfcaaaa6
Trevan <10931268@uvu.edu>
2025-10-30 20:38:20 -0600

    Began modeling table model
    Forgot to commit this the other day. Began modeling a rustic complex table prop.

---
+++ Unity Projects/My Project/Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerInputs
+{
+    private void Update()
+    {
+
+    }
+
+    private void KeysPressed()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+
+    }
+}
