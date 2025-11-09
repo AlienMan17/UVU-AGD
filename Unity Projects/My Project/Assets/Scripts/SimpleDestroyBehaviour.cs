0d097a7adbe8c89ce92c393bde0d89b038d7d5f5
Trevan <10931268@uvu.edu>
2025-11-08 20:36:09 -0700

    Set up most of the base assets to the TD game & Knightfall retopo
    Set up most of what I would need to have a good foundation for the tower defense game. That would be setting up the camera, baking the navmesh surface onto a path, configuring the AI enemy to navigate, set up basic models/materials, and finally started the UI creation. Then I did some more work on the Knightfall retopo.

---
+++ Unity Projects/My Project/Assets/Scripts/SimpleDestroyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SimpleDestroyBehaviour : MonoBehaviour
+{
+	public void DestroyObj()
+	{
+		Destroy(gameObject);
+	}
+
+	public void DestroyAny(Object obj)
+	{
+		Destroy(obj);
+    }
+}
